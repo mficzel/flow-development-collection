mficzel/flow-development-collection

commit 163e204df2b724555af2c2a6fd800bd32eb0b95c

TASK: Replace some deprecated methods usage

Neos.Flow/Classes/Package.php
@@ -11,6 +11,8 @@ namespace Neos\Flow;
  * source code.
  */
 
+use Neos\Flow\Core\Booting\Step;
+use Neos\Flow\Http\Helper\SecurityHelper;
 use Neos\Flow\Package\Package as BasePackage;
 use Neos\Flow\Package\PackageManager;
 use Neos\Flow\ResourceManagement\ResourceManager;
@@ -53,10 +55,10 @@ class Package extends BasePackage
 
         $dispatcher = $bootstrap->getSignalSlotDispatcher();
         $dispatcher->connect(Mvc\Dispatcher::class, 'afterControllerInvocation', function ($request) use ($bootstrap) {
-            if ($bootstrap->getObjectManager()->hasInstance(Persistence\PersistenceManagerInterface::class)) {
-                if (!$request instanceof Mvc\ActionRequest || $request->getHttpRequest()->isMethodSafe() !== true) {
+            if ($bootstrap->getObjectManager()->has(Persistence\PersistenceManagerInterface::class)) {
+                if (!$request instanceof Mvc\ActionRequest || SecurityHelper::hasSafeMethod($request->getHttpRequest()) !== true) {
                     $bootstrap->getObjectManager()->get(Persistence\PersistenceManagerInterface::class)->persistAll();
-                } elseif ($request->getHttpRequest()->isMethodSafe()) {
+                } elseif (SecurityHelper::hasSafeMethod($request->getHttpRequest())) {
                     $bootstrap->getObjectManager()->get(Persistence\PersistenceManagerInterface::class)->persistAll(true);
                 }
             }
@@ -64,7 +66,7 @@ class Package extends BasePackage
         $dispatcher->connect(Cli\SlaveRequestHandler::class, 'dispatchedCommandLineSlaveRequest', Persistence\PersistenceManagerInterface::class, 'persistAll');
 
         if (!$context->isProduction()) {
-            $dispatcher->connect(Core\Booting\Sequence::class, 'afterInvokeStep', function ($step) use ($bootstrap, $dispatcher) {
+            $dispatcher->connect(Core\Booting\Sequence::class, 'afterInvokeStep', function (Step $step) use ($bootstrap, $dispatcher) {
                 if ($step->getIdentifier() === 'neos.flow:resources') {
                     $publicResourcesFileMonitor = Monitor\FileMonitor::createFileMonitorAtBoot('Flow_PublicResourcesFiles', $bootstrap);
                     /** @var PackageManager $packageManager */
@@ -84,7 +86,7 @@ class Package extends BasePackage
                 }
             });
 
-            $publishResources = function ($identifier, $changedFiles) use ($bootstrap) {
+            $publishResources = function ($identifier) use ($bootstrap) {
                 if ($identifier !== 'Flow_PublicResourcesFiles') {
                     return;
                 }
@@ -127,7 +129,7 @@ class Package extends BasePackage
 
         $dispatcher->connect(Persistence\Doctrine\EntityManagerFactory::class, 'beforeDoctrineEntityManagerCreation', Persistence\Doctrine\EntityManagerConfiguration::class, 'configureEntityManager');
         $dispatcher->connect(Persistence\Doctrine\EntityManagerFactory::class, 'afterDoctrineEntityManagerCreation', Persistence\Doctrine\EntityManagerConfiguration::class, 'enhanceEntityManager');
- 
+
         $dispatcher->connect(Persistence\Doctrine\PersistenceManager::class, 'allObjectsPersisted', ResourceRepository::class, 'resetAfterPersistingChanges');
         $dispatcher->connect(Persistence\Generic\PersistenceManager::class, 'allObjectsPersisted', ResourceRepository::class, 'resetAfterPersistingChanges');
     }

Neos.Flow/Classes/Error/DebugExceptionHandler.php
@@ -14,6 +14,7 @@ namespace Neos\Flow\Error;
 use Neos\Flow\Annotations as Flow;
 use Neos\Flow\Core\Bootstrap;
 use Neos\Flow\Exception as FlowException;
+use Neos\Flow\Http\Helper\ResponseInformationHelper;
 use Neos\Flow\Http\Response;
 use Neos\Flow\ObjectManagement\ObjectManagerInterface;
 
@@ -63,7 +64,7 @@ EOD;
         if ($exception instanceof FlowException) {
             $statusCode = $exception->getStatusCode();
         }
-        $statusMessage = Response::getStatusMessageByCode($statusCode);
+        $statusMessage = ResponseInformationHelper::getStatusMessageByCode($statusCode);
         if (!headers_sent()) {
             header(sprintf('HTTP/1.1 %s %s', $statusCode, $statusMessage));
         }
@@ -91,7 +92,7 @@ EOD;
      */
     protected function renderStatically(int $statusCode, \Throwable $exception)
     {
-        $statusMessage = Response::getStatusMessageByCode($statusCode);
+        $statusMessage = ResponseInformationHelper::getStatusMessageByCode($statusCode);
         $exceptionHeader = '<div class="Flow-Debug-Exception-Header">';
         while (true) {
             $filepaths = Debugger::findProxyAndShortFilePath($exception->getFile());

Neos.Flow/Classes/Error/ProductionExceptionHandler.php
@@ -13,6 +13,7 @@ namespace Neos\Flow\Error;
 
 use Neos\Flow\Annotations as Flow;
 use Neos\Flow\Exception as FlowException;
+use Neos\Flow\Http\Helper\ResponseInformationHelper;
 use Neos\Flow\Http\Response;
 use Psr\Log\LogLevel;
 
@@ -35,7 +36,7 @@ class ProductionExceptionHandler extends AbstractExceptionHandler
         if ($exception instanceof FlowException) {
             $statusCode = $exception->getStatusCode();
         }
-        $statusMessage = Response::getStatusMessageByCode($statusCode);
+        $statusMessage = ResponseInformationHelper::getStatusMessageByCode($statusCode);
         $referenceCode = ($exception instanceof FlowException) ? $exception->getReferenceCode() : null;
         if (!headers_sent()) {
             header(sprintf('HTTP/1.1 %s %s', $statusCode, $statusMessage));
@@ -68,7 +69,7 @@ class ProductionExceptionHandler extends AbstractExceptionHandler
      */
     protected function renderStatically(int $statusCode, ?string $referenceCode): string
     {
-        $statusMessage = Response::getStatusMessageByCode($statusCode);
+        $statusMessage = ResponseInformationHelper::getStatusMessageByCode($statusCode);
         $referenceCodeMessage = ($referenceCode !== null) ? '<p>When contacting the maintainer of this application please mention the following reference code:<br /><br />' . $referenceCode . '</p>' : '';
 
         return '<!DOCTYPE html>

Neos.Flow/Classes/Security/RequestPattern/CsrfProtection.php
@@ -12,6 +12,7 @@ namespace Neos\Flow\Security\RequestPattern;
  */
 
 use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Http\Helper\SecurityHelper;
 use Neos\Flow\Mvc\ActionRequest;
 use Neos\Flow\Mvc\RequestInterface;
 use Neos\Flow\ObjectManagement\ObjectManagerInterface;
@@ -86,7 +87,7 @@ class CsrfProtection implements RequestPatternInterface
      */
     public function matchRequest(RequestInterface $request)
     {
-        if (!$request instanceof ActionRequest || $request->getHttpRequest()->isMethodSafe()) {
+        if (!$request instanceof ActionRequest || SecurityHelper::hasSafeMethod($request->getHttpRequest())) {
             $this->logger->debug('CSRF: No token required, safe request');
             return false;
         }

commit 3b227ea3110b897df5bda33b5143d51c29cdb174

BUGFIX: Prevent premature connection to database before `PersistenceManager::persistAllowedObjects`
For every simple GET request `persistAllowedObjects` is called via the Package.php in flow.
This results in building a db connection, even if there is nothing to do.

So if neither the persistentManger nor the entityManager has been created by the object manager instantiating one and thus the other which leads to "work".

For the check if the `entityManager` `isOpen` we activate the lazy dependency.

Now in this retrieval process - even though doctrines connection is lazy itself - the connection will be made forcefully
see `\Neos\Flow\Persistence\Doctrine\EntityManagerFactory::create` line 120 (https://github.com/neos/flow-development-collection/blob/11e2348125dd8286ff9ccc088e5d187dc9143bf5/Neos.Flow/Classes/Persistence/Doctrine/EntityManagerFactory.php#L120) or https://github.com/neos/flow-development-collection/blob/d93b6b09ca2071c87812a9ef4bc120201c44608a/Neos.Flow/Classes/Persistence/Doctrine/EntityManagerConfiguration.php#L229

This is ironic because if there is no connection - or no entity manager and persistence manager in the first place, the current process cannot have made any changes to the transaction.

Reason for this is that the Package.php has a wrong check which looks for if the PersistenceManager _exists_ as php class at all rather than if we have an active instance loaded.
This is an old regression from: https://github.com/neos/flow-development-collection/commit/163e204df2b724555af2c2a6fd800bd32eb0b95c

Neos.Flow/Classes/Package.php
@@ -66,9 +66,11 @@ class Package extends BasePackage
         $dispatcher = $bootstrap->getSignalSlotDispatcher();
 
         $dispatcher->connect(Mvc\Dispatcher::class, 'afterControllerInvocation', function ($request) use ($bootstrap) {
-            // No auto-persistence if there is no PersistenceManager registered
+            // No auto-persistence if there is no PersistenceManager loaded
+            // This signal will not be fired at compile time, as it's only emitted for web requests which happen at runtime.
             if (
-                $bootstrap->getObjectManager()->has(Persistence\PersistenceManagerInterface::class)
+                /** @phpstan-ignore-next-line the object manager interface doesn't specify this method */
+                $bootstrap->getObjectManager()->hasInstance(Persistence\PersistenceManagerInterface::class)
             ) {
                 if (!$request instanceof Mvc\ActionRequest || SecurityHelper::hasSafeMethod($request->getHttpRequest()) !== true) {
                     $bootstrap->getObjectManager()->get(Persistence\PersistenceManagerInterface::class)->persistAll();